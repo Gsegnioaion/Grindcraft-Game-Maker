f53c862df8730fb6b6cf03ed690b231c2ca7a351
Gsegnioaion <knook.knooks@outlook.com>
2025-06-20 19:44:40 +1000

    Added a shit ton

--- js/areas.js
+++ js/areas.js
@@ -36,6 +36,42 @@ addResources({                                          // Function for adding a
     axe: {
         image: "images/axe.png",
     },
+    gravel: {
+        image: "images/gravel.png",
+    },
+    bone: {
+        image: "images/bone.png",
+    },
+    "oak log": {
+        image: "images/oaklog.png",
+    },
+    bark: {
+        image: "images/bark.png",
+    },
+    mushroom: {
+        image: "images/mushroom.png",
+    },
+    flint: {
+        image: "images/flint.png",
+    },
+    grindstone: {
+        image: "images/grindstone.png",
+    },
+    bowl: {
+        image: "images/bowl.png",
+    },
+    water: {
+        image: "images/water.png",
+    },
+    leaves: {
+        image: "images/leaves.png",
+    },
+    "dry clay": {
+        image: "images/dryclay.png",
+    },
+    "sugar cane": {
+        image: "images/sugarcane.png",
+    },
 });
 
 // Areas
@@ -56,22 +92,76 @@ addArea("c",                                            // Function for adding a
                 {
                     id: "stick",
                     time: [["basket", 0.3], ["", 0.5]],
-                    probability: 20,
+                    probability: 15,
                 },
                 {
                     id: "grass",
                     time: [["", 0.6]],
-                    probability: 30,
+                    probability: 25,
                 },
                 {
                     id: "stone",
                     time: [["basket", 0.5], ["", 0.7]],
-                    probability: 30,
+                    probability: 15,
                 },
                 {
                     id: "dirt",
                     time: [["shovel", 0.6]],
+                    probability: 15,
+                },
+                {
+                    id: "gravel",
+                    time: [["shovel", 0.7]],
+                    probability: 15,
+                },
+                {
+                    id: "bone",
+                    time: [["shovel", 0.9]],
+                    probability: 15,
+                },
+            ]
+        },
+        {
+            name: "forest",
+            unlocked: false,
+            background: "images/grinds/forest.png",
+            resources: [
+                {
+                    id: "oak log",
+                    time: [["axe", 0.8]],
+                    probability: 60,
+                },
+                {
+                    id: "bark",
+                    time: [["axe", 0.6]],
+                    probability: 40,
+                },
+            ]
+        },
+        {
+            name: "river",
+            unlocked: false,
+            background: "images/grinds/river.png",
+            resources: [
+                {
+                    id: "water",
+                    time: [["bowl", 1]],
+                    probability: 40,
+                },
+                {
+                    id: "leaves",
+                    time: [["bowl", 0.5]],
                     probability: 20,
+                },
+                {
+                    id: "dry clay",
+                    time: [["bowl", 1.2]],
+                    probability: 20,
+                },
+                {
+                    id: "sugar cane",
+                    time: [["bowl", 1.1]],
+                    probability: 20, 
                 },
             ]
         },
@@ -123,12 +213,83 @@ addArea("c",                                            // Function for adding a
             name: "axe",
             desc: "A powerful to cut down wood, unlocks FOREST",
             cost: [["stick", 2], ["rope", 2], ["stone", 4]],
+            unlockGrinds: "forest",
+        },
+        {
+            name: "bowl",
+            desc: "unlocks river grind",
+            cost: [["bark", 3]],
+            unlockGrinds: "river",
         },
         {
             name: "dirt",
             desc: "A pile of dirt",
             type: "display",
             cost: [["dirt", 0]],
+        },
+        {
+            name: "gravel",
+            desc: "A pile of small rocks",
+            type: "display",
+            cost: [["gravel", 0]],
+        },
+        {
+            name: "bone",
+            desc: "A remnant of something once alive",
+            type: "display",
+            cost: [["bone", 0]],
+        },
+        {
+            name: "oak log",
+            desc: "A part of something greater",
+            type: "display",
+            cost: [["oak log", 0]],
+        },
+        {
+            name: "bark",
+            desc: "Tree skin",
+            type: "display",
+            cost: [["bark", 0]],
+        },
+        {
+            name: "mushroom",
+            desc: "A small mushroom from the forest floor",
+            type: "display",
+            cost: [["mushroom", 0]],
+        },
+        {
+            name: "water",
+            desc: "Literally just water",
+            type: "display",
+            cost: [["water", 0]],
+        },
+        {
+            name: "leaves",
+            desc: "A handful of fresh leaves from a nearby tree",
+            type: "display",
+            cost: [["leaves", 0]],
+        },
+        {
+            name: "dry clay",
+            desc: "Wet clay except its dried",
+            type: "display",
+            cost: [["dry clay", 0]],
+        },
+        {
+            name: "sugar cane",
+            desc: "A cane that contains sugar",
+            type: "display",
+            cost: [["sugar cane", 0]],
+        },
+        {
+            name: "flint",
+            desc: "A small shard of something buried in gravel",
+            cost: [["gravel", 3]],
+        },
+        {
+            name: "grindstone",
+            desc: "Used to sharpen things",
+            cost: [["flint", 2], ["oak log", 1], ["stone", 5], ["stick", 5]],
         },
     ],
 
